cb58e066335e572c7d6f4208741269509be02b71
nani-09 <43917293+nani-09@users.noreply.github.com>
2020-12-01 20:22:22 -0500

    pre work python, java

---
+++ sakila-database/func_prog_class_3.sql
@@ -0,0 +1,35 @@
+SELECT first_name, last_name, email 
+FROM customer
+where email > 'w'
+order by email;
+##List all the email greater than 'w', where 'wa' is greater value than 'w'
+
+##Lower() method usage
+select lower(title), title
+from film;
+
+##sounddex() method usage : 'soundex' method converts any string value into a hexa decimal value
+select soundex(title), title 
+from film;
+
+## agv(), count(), sum() methods
+select count(title) from film;
+select count(*) from film;
+select avg(rental_rate) average_rental, avg(replacement_cost) average_replacement from film;
+## calculating the difference values
+select title, (replacement_cost - rental_rate) differnece from film;
+select avg(replacement_cost - rental_rate) differnece from film;
+
+##Grouping, distinct  
+select distinct rating from film order by rating;
+select rating, count(*) from film group by rating;
+
+# filter PRIOR to grouping
+select rating, count(*) from film where rating in ('G', 'PG', 'PG-13') group by rating;
+
+# filter after grouping
+select rating, count(*) tot_count from film group by rating having tot_count > 200;
+select rating, count(*) tot_count from film where rating in ('G', 'PG', 'PG-13') group by rating having tot_count > 200;
+
+SQL joins
+
